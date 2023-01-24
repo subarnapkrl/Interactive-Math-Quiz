0886887047c17e28a663a43f106d351eaae9195e
Soobarna <subarnapokharel252@gmail.com>
2023-01-24 17:59:01 +0545

    Calculation-Logic-Done

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let questionElement=document.querySelector(".question");
-let ourForm=document.querySelector(".our-form")
+let ourForm=document.querySelector(".our-form");
+let ourField=document.querySelector(".our-field");
+let pointsNeeded=document.querySelector(".points-needed");
+let mistakesAllowed=document.querySelector(".mistakes-allowed")
 
 let state={
     score:0,
     wrongAnswers:0,
-    currentQuestion:generateQuestions()
+    // currentQuestion:generateQuestions()
 }
 
 function generateRandomNumber(maxVal)
 {
-    return Math.floor(Math.random()*(maxVal+1))
+    return Math.floor(Math.random()*(maxVal+1));
 }
 
 function generateQuestions()
@@ -24,7 +27,7 @@ function generateQuestions()
 
 function updateProblem()
 {
-    // state.currentQuestion=generateQuestions() //Storing the current question displayed on the screen in memory named "state"
+    state.currentQuestion=generateQuestions() //Storing the current question displayed on the screen in memory named "state"
     questionElement.innerHTML=`${state.currentQuestion.num1} ${state.currentQuestion.operator} ${state.currentQuestion.num2}`
 
 }
@@ -34,4 +37,22 @@ updateProblem()
 ourForm.addEventListener("submit",function(e)
 {
     e.preventDefault();
+    let correctAnswer;
+
+    if(state.currentQuestion.operator=="+") correctAnswer=state.currentQuestion.num1+state.currentQuestion.num2;
+    if(state.currentQuestion.operator=="-") correctAnswer=state.currentQuestion.num1-state.currentQuestion.num2;
+    if(state.currentQuestion.operator=="x") correctAnswer=state.currentQuestion.num1*state.currentQuestion.num2;
+
+    if(parseInt(ourField.value)==correctAnswer)
+    {
+        state.score++;
+        pointsNeeded.textContent=10-state.score;
+        updateProblem();
+        ourField.value=""
+        ourField.focus()
+
+    }else{
+         state.wrongAnswers++;
+         mistakesAllowed.textContent=2-state.wrongAnswers;
+    }
 })
